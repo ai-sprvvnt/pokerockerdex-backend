c44da4a8b383fb61cb39692a1ad13139fe5100da
Felipe G <ai.sprvvnt@gmail.com>
2026-08-15 10:57:39 -0600

    feat: agregar logging de solicitudes y errores

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const NotFoundError = require('./errors/NotFoundError');
 const errorHandler = require('./middlewares/error-handler');
+
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
 const { PORT, databaseAddress } = require('./utils/config');
 
 const app = express();
@@ -12,12 +15,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(requestLogger);
+
 app.use('/', routes);
 
 app.use((req, res, next) => {
   next(new NotFoundError());
 });
 
+app.use(errorLogger);
 app.use(errorHandler);
 
 mongoose
