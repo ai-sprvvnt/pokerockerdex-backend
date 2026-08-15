45e6e61b4cf4454200cc6cd1d2f6c6ac66143780
Felipe G <ai.sprvvnt@gmail.com>
2026-08-14 20:02:32 -0600

    feat: agregar eliminacion de pokemon con ownership

--- controllers/teams.js
+++ controllers/teams.js
@@ -1,6 +1,8 @@
 const Pokemon = require('../models/pokemon');
 
 const ConflictError = require('../errors/ConflictError');
+const ForbiddenError = require('../errors/ForbiddenError');
+const NotFoundError = require('../errors/NotFoundError');
 
 const {
   DUPLICATE_KEY_ERROR_CODE,
@@ -27,10 +29,7 @@ const getTeam = (req, res, next) => Pokemon.find({
 
 const addPokemon = async (req, res, next) => {
   const {
-    id,
-    name,
-    image,
-    types,
+    id, name, image, types,
   } = req.body;
 
   try {
@@ -48,9 +47,7 @@ const addPokemon = async (req, res, next) => {
     });
 
     if (teamSize >= MAX_TEAM_SIZE) {
-      throw new ConflictError(
-        'El equipo ya tiene el máximo de 6 Pokémon.',
-      );
+      throw new ConflictError('El equipo ya tiene el máximo de 6 Pokémon.');
     }
 
     const pokemon = await Pokemon.create({
@@ -68,11 +65,39 @@ const addPokemon = async (req, res, next) => {
     });
   } catch (err) {
     if (err.code === DUPLICATE_KEY_ERROR_CODE) {
-      return next(
-        new ConflictError('Este Pokémon ya pertenece al equipo.'),
-      );
+      return next(new ConflictError('Este Pokémon ya pertenece al equipo.'));
     }
 
+    return next(err);
+  }
+};
+
+const deletePokemon = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const pokemon = await Pokemon.findById(id).select('+owner');
+
+    if (!pokemon) {
+      throw new NotFoundError('Pokémon no encontrado.');
+    }
+
+    if (!pokemon.owner.equals(req.user._id)) {
+      throw new ForbiddenError('No tienes permiso para eliminar este Pokémon.');
+    }
+
+    await pokemon.deleteOne();
+
+    const teamSize = await Pokemon.countDocuments({
+      owner: req.user._id,
+    });
+
+    return res.status(200).send({
+      pokemon: formatPokemon(pokemon),
+      teamSize,
+      maxTeamSize: MAX_TEAM_SIZE,
+    });
+  } catch (err) {
     return next(err);
   }
 };
@@ -80,4 +105,5 @@ const addPokemon = async (req, res, next) => {
 module.exports = {
   getTeam,
   addPokemon,
+  deletePokemon,
 };
--- routes/teams.js
+++ routes/teams.js
@@ -1,11 +1,16 @@
 const router = require('express').Router();
 
-const { getTeam, addPokemon } = require('../controllers/teams');
+const { getTeam, addPokemon, deletePokemon } = require('../controllers/teams');
 
-const { validateCreatePokemon } = require('../utils/validation');
+const {
+  validateCreatePokemon,
+  validatePokemonId,
+} = require('../utils/validation');
 
 router.get('/', getTeam);
 
 router.post('/pokemon', validateCreatePokemon, addPokemon);
+
+router.delete('/pokemon/:id', validatePokemonId, deletePokemon);
 
 module.exports = router;
