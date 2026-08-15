86a169bbf06dbc3950383108819dc67743eec238
Felipe G <ai.sprvvnt@gmail.com>
2026-08-15 11:06:36 -0600

    feat: agregar medidas de seguridad al backend

--- app.js
+++ app.js
@@ -2,20 +2,25 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+const helmet = require('helmet');
 const routes = require('./routes');
 const NotFoundError = require('./errors/NotFoundError');
 const errorHandler = require('./middlewares/error-handler');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
+const apiLimiter = require('./middlewares/rate-limiter');
+
 const { PORT, databaseAddress } = require('./utils/config');
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
 app.use(requestLogger);
+app.use(apiLimiter);
 
 app.use('/', routes);
 
