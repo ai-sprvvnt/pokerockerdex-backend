e0bcbf4424612726d25835b9264c2f1c28fcbc54
Felipe G <ai.sprvvnt@gmail.com>
2026-08-18 11:06:35 -0600

    fix: establecer longitud minima de contraseña

--- utils/validation.js
+++ utils/validation.js
@@ -3,7 +3,7 @@ const { celebrate, Joi, Segments } = require('celebrate');
 const validateSignup = celebrate({
   [Segments.BODY]: Joi.object().keys({
     email: Joi.string().email().required(),
-    password: Joi.string().required(),
+    password: Joi.string().min(8).required(),
     name: Joi.string().min(2).max(30).required(),
   }),
 });
